fc4529f387e33a37813b4fc8b0a2fe5a2e5e9d68
Sean1127 <wannaplay1304@gmail.com>
2017-02-25 15:53:01 +0800

    Clone "phonebook", attempt no.1 to lower cache miss rate
    the homework is to review data structure and computer architecture
    then revise the phonebook_opt.[ch] to speed up and minimize cache miss
    attempt no.1 redesign a smaller structure for two and only two functions:
    findName()
    append()

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -3,9 +3,8 @@
 
 #define MAX_LAST_NAME_SIZE 16
 
-/* TODO: After modifying the original version, uncomment the following
- * line to set OPT properly */
-// #define OPT 1
+#define OPT 1
+
 typedef struct __PHONE_BOOK_ENTRY {
     char lastName[MAX_LAST_NAME_SIZE];
     char firstName[16];
@@ -18,6 +17,12 @@ typedef struct __PHONE_BOOK_ENTRY {
     char state[2];
     char zip[5];
     struct __PHONE_BOOK_ENTRY *pNext;
+} full;
+
+typedef struct __PHONE_BOOK_LASTNAME {
+    char lastName[MAX_LAST_NAME_SIZE];
+    struct __PHONE_BOOK_ENTRY *pFull;
+    struct __PHONE_BOOK_LASTNAME *pNext;
 } entry;
 
 entry *findName(char lastName[], entry *pHead);
